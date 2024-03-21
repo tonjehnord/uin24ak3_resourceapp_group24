9cbcf7b86ea18e75441e632133c311f7e0add85f
NYM00 <victor.nymo@gmail.com>
2024-03-21 11:44:06 +0100

    Ferdigstillt Resources. Lagt inn ressurser under assets.

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,17 +1,30 @@
-
+import { useEffect, useState } from 'react'
 import { resources } from '../assets/ressurser'
+import { useParams } from 'react-router-dom'
+
 
 export default function Resources() {
 
-    return(
+    const { category } = useParams()
+    const [filteredResources, setFilteredResources] = useState([])
+
+    useEffect(() => {
+        const filtered = resources.filter(resource => resource.category === category)
+        setFilteredResources(filtered)
+    }, [category])
+
+    return (
     <>
         <article>
             <ul>
-                <li>
-                    <a href="#"></a>
-                </li>
+                {filteredResources.map((resource, index) => (
+                    <li key={index}>
+                        <a href={resource.url}>{resource.title}</a>
+                    </li>
+                ))}
             </ul>
         </article>
     </>
     )
+
 }
