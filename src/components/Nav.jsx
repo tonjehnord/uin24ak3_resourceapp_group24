499720a17cef0b33000b43208461a53b4f064add
tonjehnord <tonjnord@gmail.com>
2024-03-21 11:36:44 +0100

    lagt inn router i main / lagt inn nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,28 @@
+import { Link } from "react-router-dom"
 
 export default function Nav() {
+  
+  const navList = [
+      { name: "HTML", url: "html" },
+      { name: "CSS", url: "css" },
+      { name: "JavaScript", url: "javascript" },
+      { name: "React", url: "react" },
+      { name: "Sanity and headless CMS", url: "headless-cms" }
+  ]
 
   return (
     <nav>
       <ul>
-          <li>
+        {navList.map((category, i) => (
+          <li key={i}>
+            <Link to={`${category.url}`}>
+              {category.name}
+            </Link>
           </li>
+        ))}
       </ul>
     </nav>
   )
 }
+
+
